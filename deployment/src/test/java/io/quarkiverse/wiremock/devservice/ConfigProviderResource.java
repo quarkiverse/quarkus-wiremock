a2c79f6f190ffeda9d8cbb3c2b7dfbcf9f607007
Christian Berger <chberger@users.noreply.github.com>
2023-10-17 16:27:08 +0200

    feat: Upgrade Quarkus version to 3.2.6.Final (#45)

--- deployment/src/test/java/io/quarkiverse/wiremock/devservice/ConfigProviderResource.java
+++ deployment/src/test/java/io/quarkiverse/wiremock/devservice/ConfigProviderResource.java
@@ -2,12 +2,12 @@ package io.quarkiverse.wiremock.devservice;
 
 import static io.quarkiverse.wiremock.devservice.WireMockDevServiceConfig.PREFIX;
 
-import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.config.ConfigProvider;
 
