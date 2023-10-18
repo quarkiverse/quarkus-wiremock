c6039cc82d15b5ea3509cb2c8c7de3673c0d0e8c
Willem Jan Glerum <willem@wjglerum.nl>
2023-10-18 19:19:51 +0200

    feat: enable global response templating (#52)

--- deployment/src/main/java/io/quarkiverse/wiremock/devservice/WireMockServerProcessor.java
+++ deployment/src/main/java/io/quarkiverse/wiremock/devservice/WireMockServerProcessor.java
@@ -10,6 +10,7 @@ import java.io.UncheckedIOException;
 import org.jboss.logging.Logger;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration;
 
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.CuratedApplicationShutdownBuildItem;
@@ -54,8 +55,11 @@ class WireMockServerProcessor {
     private static RunningDevService startWireMockDevService(WireMockServerConfig config) {
 
         LOGGER.debugf("Starting WireMock server with port [%s] and path [%s]", config.port(), config.filesMapping());
-        final WireMockServer server = new WireMockServer(
-                options().port(config.port()).usingFilesUnderDirectory(config.filesMapping()));
+        final WireMockConfiguration configuration = options()
+                .port(config.port())
+                .usingFilesUnderDirectory(config.filesMapping())
+                .globalTemplating(config.globalResponseTemplating());
+        final WireMockServer server = new WireMockServer(configuration);
         server.start();
 
         return new RunningDevService(config.serviceName(), null, server::shutdown, getPropertyKey(PORT),
