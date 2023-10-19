76631cf629309d57f2d8a62dd88a2ba577a49c08
Willem Jan Glerum <willem@wjglerum.nl>
2023-10-19 12:15:24 +0200

    feat: switch to wiremock-jre8-standalone
    WireMock pulls in a bunch of (vulnerable) dependencies that we don't need, so simplifying the dependency tree by switching to the standalone version, see https://wiremock.org/docs/standalone/
    
    Quarkus also uses the standalone version in their tests, see for example https://github.com/quarkusio/quarkus/blob/7dcbe6793d431ce140131550775c406ea9e2d74e/extensions/resteasy-reactive/rest-client-reactive/deployment/pom.xml#L73
    
    I'll do the same for the main branch next.

--- deployment/src/test/java/io/quarkiverse/wiremock/devservice/WireMockBasicTest.java
+++ deployment/src/test/java/io/quarkiverse/wiremock/devservice/WireMockBasicTest.java
@@ -4,8 +4,8 @@ import static io.quarkiverse.wiremock.devservice.TestUtil.APP_PROPERTIES;
 import static io.quarkiverse.wiremock.devservice.WireMockDevServiceConfig.PORT;
 import static io.quarkiverse.wiremock.devservice.WireMockDevServiceConfig.PREFIX;
 import static org.hamcrest.Matchers.is;
+import static org.jboss.resteasy.reactive.RestResponse.StatusCode.OK;
 
-import org.eclipse.jetty.server.Response;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
@@ -24,14 +24,14 @@ class WireMockBasicTest {
     @Test
     void testWireMockMappingsFolder() {
         final int port = ConfigProvider.getConfig().getValue(PREFIX + "." + PORT, Integer.class);
-        RestAssured.when().get(String.format("http://localhost:%d/wiremock", port)).then().statusCode(Response.SC_OK)
+        RestAssured.when().get(String.format("http://localhost:%d/wiremock", port)).then().statusCode(OK)
                 .body(is("Everything was just fine!"));
     }
 
     @Test
     void testTemplatingDisabled() {
         final int port = ConfigProvider.getConfig().getValue(PREFIX + "." + PORT, Integer.class);
-        RestAssured.when().get(String.format("http://localhost:%d/template", port)).then().statusCode(Response.SC_OK)
+        RestAssured.when().get(String.format("http://localhost:%d/template", port)).then().statusCode(OK)
                 .body(is("Everything was just fine from {{ request.port }}!"));
     }
 }
